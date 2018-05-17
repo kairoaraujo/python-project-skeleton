b8748d3b3f0c90940faef17198e5eef00ca1e5b6
Kairo Araujo <kairo@kairo.eti.br>
2018-05-17 11:23:10 +0200

    Added better version handling + headers

--- project_name/__init__.py
+++ project_name/__init__.py
@@ -1,2 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Author: Kairo Araujo <kairo@kairo.eti.br>
+#
+
+from os.path import dirname, join
+
+with open(join(dirname('..'), 'VERSION'), 'rb') as f:
+    version = f.read().decode('ascii').strip()
--- setup.py
+++ setup.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
+# Author: Kairo Araujo <kairo@kairo.eti.br>
+#
 
 from setuptools import setup, find_packages
+from project_name import version
 
 setup(
     name='project_name',
-    version='1.0',
+    version=version,
     long_description=__doc__,
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
-    # install_requires=['Flask'] if Flask
+    install_requires=['Flask']  # if Flask project
 )
