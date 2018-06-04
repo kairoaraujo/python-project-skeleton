6584186029abd1e4fc7662d0ffdfcad22a3112a9
Kairo Araujo <kairo@kairo.eti.br>
2018-06-04 12:48:20 +0200

    Working framework to make easy to start. Added new flask_restplus

--- app.py
+++ app.py
@@ -5,14 +5,8 @@
 from flask import Flask
 from flask_restplus import Api
 from project_name import version
-from project_name import ns as ns_method
+from project_name.api.v1.project import ns as ns_method
 import logging
-
-app = Flask(__name__)
-api = Api(
-    app, version=version, title='Project Name API',
-    description='A Project description of API'
-)
 
 logging.basicConfig(
     level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s',
@@ -21,4 +15,11 @@ logging.basicConfig(
     level=logging.INFO, format='%(asctime)s %(levelname)s %(message)s',
     datefmt='%H:%M:%S')
 
-api.add_namespace(ns_method, path="/api")
+app = Flask(__name__)
+api = Api(
+    app, version=version, title='Project Name API',
+    description='A Project description of API'
+)
+
+
+api.add_namespace(ns_method, path="/api/project")
