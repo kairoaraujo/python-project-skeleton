825e39f6255d568550ffe9c79fa852750355b502
Kairo Araujo <kairo@kairo.eti.br>
2018-09-02 18:10:54 +0200

    Ignoring files as required

--- project_name/constants/database.py
+++ project_name/constants/database.py
@@ -7,6 +7,6 @@ import os
 
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__name__))
 DATABASE = os.path.join(
-    PROJECT_ROOT, "project_name", "databases", "apikeys.db")
+    PROJECT_ROOT, "project_name", "databases", "project_name.db")
 SQLALCHEMY_DATABASE_URI = "sqlite:///" + DATABASE
 SQLALCHEMY_TRACK_MODIFICATIONS = False
